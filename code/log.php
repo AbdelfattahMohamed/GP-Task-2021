1c85fd1c6672199b54b2eb59b5abe206a31a533d
tarek yahia <tarek_500@yahoo.com>
2020-06-28 05:00:02 +0200

    log in

---
+++ code/log.php
@@ -0,0 +1,39 @@
+<?php
+
+   $c = mysqli_connect('localhost','root','','clinic');
+   
+   if(isset($_POST['username']))
+   {
+      $name =$_POST['username'];
+      $pass  = $_POST['pass'];
+      $sql = "select * from users where email = '".$name."'AND password = '".$pass."' limit 1  ";
+      $sqlrole = "select * from users where email = '".$name."'AND password = '".$pass."' AND role = '". 1 ."' limit 1  ";
+      $result = mysqli_query($c,$sql);
+      $resultrole = mysqli_query($c,$sqlrole);
+   
+
+     
+      if (mysqli_num_rows($result) == 1 )
+      {
+         
+              
+         if (mysqli_num_rows($resultrole) == 1){
+           header("Location: \GP-Task-2021/pages/doctor.html");
+           exit;
+         }
+         else {
+            header("Location:  \GP-Task-2021/pages/pharmacist.html");
+            exit;
+         }
+         
+      }
+
+      else {
+          print("errore");
+      }
+      
+      
+     
+   }
+
+?>
